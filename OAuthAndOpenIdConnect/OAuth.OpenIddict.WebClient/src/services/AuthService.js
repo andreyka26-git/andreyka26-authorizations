86cd485ae18465c8de06383fc632de7cb2eaba61
Andrii Bui <andriibui@microsoft.com>
2023-12-21 18:34:44 +0100

    added github settings

--- OAuthAndOpenIdConnect/OAuth.OpenIddict.WebClient/src/services/AuthService.js
+++ OAuthAndOpenIdConnect/OAuth.OpenIddict.WebClient/src/services/AuthService.js
@@ -1,17 +1,38 @@
-import { UserManager } from 'oidc-client';
+import { UserManager } from 'oidc-client-ts';
+
+// custom settings that work with our ouwn OAuth server
+// const settings = {
+//     authority: 'https://localhost:7000',
+//     client_id: 'react-client',
+//     client_secret: '901564A5-E7FE-42CB-B10D-61EF6A8F3654',
+//     redirect_uri: 'http://localhost:3000/oauth/callback',
+//     silent_redirect_uri: 'http://localhost:3000/oauth/callback',
+//     post_logout_redirect_uri: 'http://localhost:3000/',
+//     response_type: 'code',
+//     // this is for getting user.profile data, when open id connect is implemented
+//     //scope: 'api1 openid profile'
+//     // this is just for OAuth2 flow
+//     scope: 'api1'
+// };
 
 const settings = {
-    authority: 'https://localhost:7000',
-    client_id: 'react-client',
-    client_secret: '901564A5-E7FE-42CB-B10D-61EF6A8F3654',
+    authority: 'https://github.com/login/oauth/authorize',
+    client_id: '',
+    client_secret: '',
     redirect_uri: 'http://localhost:3000/oauth/callback',
     silent_redirect_uri: 'http://localhost:3000/oauth/callback',
     post_logout_redirect_uri: 'http://localhost:3000/',
     response_type: 'code',
+    metadata: {
+        issuer: 'https://github.com',
+        authorization_endpoint: 'https://github.com/login/oauth/authorize',
+        userinfo_endpoint: 'https://github.com/login/oauth/authorize',
+        end_session_endpoint: 'https://github.com/login/oauth/authorize',
+        token_endpoint: 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token'
+    }
     // this is for getting user.profile data, when open id connect is implemented
     //scope: 'api1 openid profile'
     // this is just for OAuth2 flow
-    scope: 'api1'
 };
 
 const userManager = new UserManager(settings);
